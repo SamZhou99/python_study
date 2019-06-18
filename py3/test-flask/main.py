52b520dc690d9bf5abffda75392d224547a5c065
sam.z <45636987@qq.com>
2019-06-19 00:48:49 +0800

    update

--- py3/test-flask/main.py
+++ py3/test-flask/main.py
@@ -2,40 +2,44 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import redirect
+from flask import render_template
+from flask import abort
+import random
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def indexController():
-    return '''
-    <h1>Hello World !</h1>
-    <p>%s</p>
-    ''' % request.headers.get('User-Agent')
+def index():
+    return render_template('index.html', title="request.headers.get('User-Agent')")
 
 
 @app.route('/user/<name>')
 def user(name):
     if not name:
         abort(404)
-    return '<h1>My name is %s</h1>' % name
+    return render_template('index.html', title=name)
 
 
 @app.route('/cookie/<act>')
-def cookieController(act):
-    if act == 'show':
-        response = make_response('<h1>show!</h1>')
-        return response
+def cookie(act):
     if act == 'set':
-        response = make_response('<h1>set!</h1>')
-        response.set_cookie('test_cookie', '123')
+        name = 'sam_'+str(random.random())
+        response = make_response(render_template('index.html', title='set cookie name '+name))
+        response.set_cookie('name', name)
         return response
+    if act == 'get':
+        name=request.cookies.get('name')
+        if not name:
+            name = 'null'
+        return render_template('index.html', title='cookie name '+name)
 
 
 @app.route('/redirect')
-def redirectController():
+def redirect_url():
     return redirect('http://www.google.com')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
