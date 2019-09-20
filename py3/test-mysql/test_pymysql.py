1e7282756711e00d83cd268def410b25e396ccf7
sam.z <45636987@qq.com>
2019-09-20 14:59:49 +0800

    update

--- py3/test-mysql/test_pymysql.py
+++ py3/test-mysql/test_pymysql.py
@@ -1,21 +1,27 @@
 import pymysql
 
 mysql_conn = pymysql.connect(
-    host= '127.0.0.1', 
-    port= 3306, 
-    user= 'root', 
-    password= 'root', 
-    db= 'jimo100'
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    password='root',
+    db='jimo100'
 )
+
 
 def close():
     mysql_conn.close()
 
+
 def select():
-    sql = "SELECT * FROM articles WHERE a_ok='{0}' LIMIT 10".format('0')
+    sql = "SELECT a_id,a_title FROM articles ORDER BY a_id DESC LIMIT {0}".format('10')
     print(sql)
-    with mysql_conn.cursor() as cursor :
+    with mysql_conn.cursor() as cursor:
         cursor.execute(sql)
         result_list = cursor.fetchall()
         for result_one in result_list:
             print(result_one)
+
+
+select()
+close()
