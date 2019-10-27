d20a91c0c2a2c2e76b756a3710db77b65fbac53b
sam.z <45636987@qq.com>
2019-10-27 16:45:59 +0800

    update

--- py3/calculate-random.py
+++ py3/calculate-random.py
@@ -6,15 +6,49 @@ total = 0
 right = 0
 error = 0
 
+# 打印统计结果
+
 
 def print_statistical():
     global total, right, error
     print('总是题数：{}，正确：{}，错误：{}'.format(total, right, error))
     print('')
 
+# 打印正确提示
 
-def get_input_num():
+
+def print_right():
+    pass
+
+# 打印错误提示
+
+
+def print_error(resultNum):
+    pass
+
+# 获取输入文字到整数型
+
+
+def kb_input_to_int():
     return int("".join(list(filter(str.isdigit, input("等于？")))))
+
+# 加法
+
+
+def addition(n1, n2):
+    pass
+
+# 减法
+
+
+def subtraction(max, min):
+    pass
+
+# 初始化
+
+
+def init():
+    pass
 
 
 for i in range(1, max_num):
@@ -29,7 +63,7 @@ for i in range(1, max_num):
         y = min(x, y)
         print('{} - {} = ?\t'.format(x, y))
 
-    input_number = get_input_num()
+    input_number = kb_input_to_int()
     total += 1
 
     if r == 1 and x+y == input_number:
