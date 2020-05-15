1fe83208de0d6a0a3802591bd2d654792c9bd8d3
sam.z <45636987@qq.com>
2020-05-15 13:28:55 +0800

    update

--- py3/test-crawler/main.py
+++ py3/test-crawler/main.py
@@ -1,9 +1,13 @@
 import urllib.request
 import chardet
 
-response = urllib.request.urlopen('http://hsck7.com/vodplay/1314-1-1.html')
+response = urllib.request.urlopen('http://www.72dj.com/play/32853.htm')
 htmlStr = response.read()
 charset = chardet.detect(htmlStr)['encoding']
 html = htmlStr.decode(charset)
 print(charset)
 print(html)
+
+f = open('./test.html', 'w')  # 若是'wb'就表示写二进制文件
+f.write(html)
+f.close()
