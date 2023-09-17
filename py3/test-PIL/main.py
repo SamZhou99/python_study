0befde269f1265bc8fa144d2681860f87045c0bf
samzhou <45636987@qq.com>
2023-09-17 10:56:56 +0800

    update

--- py3/test-PIL/main.py
+++ py3/test-PIL/main.py
@@ -2,11 +2,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as nps
 
-image = Image.open('./test.jpg')  #读取图像
+
+# image = Image.open("./test.jpg")  # 读取图像
 # plt.imshow(image) #显示图片
 
 # plt.imshow(image.rotate(180))  #逆时针旋转180度
-plt.imshow(image.resize((64, 64)))  #缩放
+# plt.imshow(image.resize((64, 64)))  # 缩放
+
 
 # # 转换灰度图像
 # image_gray = image.convert('L')
@@ -24,4 +26,81 @@ plt.imshow(image.resize((64, 64)))  #缩放
 # data = nps.array(region)
 # plt.imshow(data)
 
-plt.show()  #需要调用show()方法，不然图像只会在内存中而不显示出来
+# plt.show()  #需要调用show()方法，不然图像只会在内存中而不显示出来
+
+from PIL import ImageDraw
+import random
+
+# from __future__ import division
+import datetime
+import pymysql
+import time
+
+config = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "root",
+    "db": "fer",
+    "charset": "utf8mb4",
+    "cursorclass": pymysql.cursors.DictCursor,
+}
+mysql_conn = pymysql.connect(**config)
+
+
+def select(limit):
+    sql = "SELECT * FROM kline_history WHERE symbol='{}' AND period='{}' ORDER BY id DESC LIMIT {}".format(
+        "btcusdt", "30min", limit
+    )
+    print(sql)
+    with mysql_conn.cursor() as cursor:
+        cursor.execute(sql)
+        return cursor.fetchall()
+        result_list = cursor.fetchall()
+        for item in result_list:
+            print(item["id"], item["close"] * 100)
+
+
+def getMaxMinNum(res_list):
+    max_num = res_list[0]["close"]
+    min_num = max_num
+    for i in res_list:
+        num = i["close"]
+        if max_num < num:
+            max_num = num
+        if min_num > num:
+            min_num = num
+    return round(max_num, 2), round(min_num, 2)
+
+
+step = 10
+res_list = select(step)
+maxNum, minNum = getMaxMinNum(res_list)
+rangeNum = maxNum - minNum
+print(maxNum, minNum, maxNum - minNum)
+imgW = 100
+imgH = 48
+img = Image.new("RGB", color=0xFFFFFF, size=(imgW, imgH))
+drawer = ImageDraw.Draw(img)
+startX = 0
+startY = (res_list[0]["close"] - minNum) / rangeNum * (imgH - 20) + 10
+index = 1
+for item in res_list:
+    x2 = index * step
+    y2 = (item["close"] - minNum) / rangeNum * (imgH - 20) + 10
+    print(index, y2)
+    drawer.line((startX, startY, x2, y2), fill="green", width=1)
+    startX = x2
+    startY = y2
+    index = index + 1
+index = 1
+startX = 0
+startY = 10
+for item in res_list:
+    x2 = index * step
+    y2 = 10 + index
+    drawer.line((startX, startY, x2, y2), fill="red", width=1)
+    startX = x2
+    startY = y2
+    index = index + 1
+img.save("./test-draw.png")
--- py3/test-pyautogui/main.py
+++ py3/test-pyautogui/main.py
@@ -4,66 +4,122 @@ from timeit import default_timer as timer
 
 startTime = timer()
 
-# # 屏幕截图
-# img = pyautogui.screenshot()  # x,y,w,h
-# img.save('screenshot.png')
+
+def screenshot():
+    # 屏幕截图
+    # w, h = 3840, 2160
+    w, h = 800, 600
+    img = pyautogui.screenshot(region=(0, 0, w, h))  # x,y,w,h
+    img.save("screenshot-2.png")
 
 
-# # 返回真实的 图片 x,y,w,h
-# findImgData = pyautogui.locateOnScreen('logo.png')
-# print(findImgData)
-# # 返回图片中心点坐标
-# sx, sy = pyautogui.center(findImgData)
-# print(sx, sy)
+def imgInfo():
+    # 返回真实的 图片 x,y,w,h
+    findImgData = pyautogui.locateOnScreen("logo.png")
+    print(findImgData)
+    # 返回图片中心点坐标
+    sx, sy = pyautogui.center(findImgData)
+    print(sx, sy)
 
 
-curr_mouse_xy = pyautogui.position()
-print(curr_mouse_xy)
+def mouseAct():
+    curr_mouse_xy = pyautogui.position()
+    print(curr_mouse_xy)
 
-screen_resolution = pyautogui.size()
-print(screen_resolution)
+    screen_resolution = pyautogui.size()
+    print(screen_resolution)
 
-num_seconds = 1
-x = 1000
-y = 100
-xOffset = 100
-yOffset = 100
-num_of_clicks = 10
-secs_between_clicks = 10
-secs_between_keys = 0.1
-moveToX = 200
-moveToY = 200
-amount_to_scroll = -10
-key_name = 'k'
+    num_seconds = 1
+    x = 1000
+    y = 100
+    xOffset = 100
+    yOffset = 100
+    num_of_clicks = 10
+    secs_between_clicks = 10
+    secs_between_keys = 0.1
+    moveToX = 200
+    moveToY = 200
+    amount_to_scroll = -10
+    key_name = "k"
 
-screen = pyautogui.onScreen(x, y)
-print(screen)
+    screen = pyautogui.onScreen(x, y)
+    print(screen)
 
-pyautogui.moveTo(x, y, duration=num_seconds)
-# pyautogui.moveRel(xOffset, yOffset, duration=num_seconds)
+    pyautogui.moveTo(x, y, duration=num_seconds)
+    # pyautogui.moveRel(xOffset, yOffset, duration=num_seconds)
 
-# pyautogui.click(x=xOffset, y=xOffset, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-# pyautogui.rightClick(x=moveToX, y=moveToY)
-# pyautogui.middleClick(x=moveToX, y=moveToY)
-# pyautogui.doubleClick(x=moveToX, y=moveToY)
-# pyautogui.tripleClick(x=moveToX, y=moveToY)
+    # pyautogui.click(x=xOffset, y=xOffset, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+    # pyautogui.rightClick(x=moveToX, y=moveToY)
+    # pyautogui.middleClick(x=moveToX, y=moveToY)
+    # pyautogui.doubleClick(x=moveToX, y=moveToY)
+    # pyautogui.tripleClick(x=moveToX, y=moveToY)
 
-# pyautogui.scroll(amount_to_scroll, x=moveToX, y=moveToY)
+    # pyautogui.scroll(amount_to_scroll, x=moveToX, y=moveToY)
 
-# pyautogui.dragTo(xOffset, yOffset, duration=num_seconds)
-# pyautogui.dragRel(xOffset+100, yOffset+100, duration=num_seconds)
+    # pyautogui.dragTo(xOffset, yOffset, duration=num_seconds)
+    # pyautogui.dragRel(xOffset+100, yOffset+100, duration=num_seconds)
 
-# pyautogui.typewrite(['a', 'b', 'c', 'left', 'backspace', 'enter', 'f1'], interval=secs_between_keys)
+    # pyautogui.typewrite(['a', 'b', 'c', 'left', 'backspace', 'enter', 'f1'], interval=secs_between_keys)
 
-# pyautogui.hotkey('ctrl', 'c')  # ctrl-c to copy
-# pyautogui.hotkey('ctrl', 'v')  # ctrl-v to paste
+    # pyautogui.hotkey('ctrl', 'c')  # ctrl-c to copy
+    # pyautogui.hotkey('ctrl', 'v')  # ctrl-v to paste
 
-# pyautogui.keyDown(key_name)
-# pyautogui.keyUp(key_name)
+    # pyautogui.keyDown(key_name)
+    # pyautogui.keyUp(key_name)
 
-pyautogui.alert('This displays some text with an OK button.')
-pyautogui.confirm('This displays text and has an OK and Cancel button.')
-pyautogui.prompt('This lets the user type in a string and press OK.')
+
+def alertModel():
+    pyautogui.alert("This displays some text with an OK button.")
+    pyautogui.confirm("This displays text and has an OK and Cancel button.")
+    pyautogui.prompt("This lets the user type in a string and press OK.")
+
+
+def isFloat(n):
+    return isinstance(n, float)
+
+
+def isInt(n):
+    return isinstance(n, int)
+
+
+def getScale(w, h):
+    len = w if w > h else h
+    i = 0
+    while len > 0:
+        i = i + 1
+        ww = w / len
+        hh = h / len
+        len = len - 1
+        if ww == int(ww) and hh == int(hh):
+            print(i, "计算次数")
+            return int(ww), int(hh)
+    return 0, 0
+
+
+def getScale2(w, h):
+    i = 0
+    isZ = w > h
+    W, H = (w, h) if isZ else (h, w)
+    w, h = W, H
+    while i < 10:
+        i = i + 1
+        m = w % h
+        print(i, w, h, m)
+        if m == 0:
+            return (int(W / h), int(H / h)) if isZ else (int(H / h), int(W / h))
+        else:
+            w = h
+            h = m
+    return 0
+
+
+# screenshot()
+
+
+# print(getScale2(3840, 2160))
+# print(getScale2(1024, 768))
+print(getScale2(15563, 19109))
+
 
 endTime = timer()
-print('耗时:' + str(endTime - startTime))
+print("耗时:" + str(endTime - startTime))
