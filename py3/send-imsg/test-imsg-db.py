b337a9da5cd63fb07ed55890c9421f0445e1a266
sam.z <45636987@qq.com>
2026-04-10 04:41:14 +0800

    update

--- py3/send-imsg/test-imsg-db.py
+++ py3/send-imsg/test-imsg-db.py
@@ -1,31 +1,62 @@
-import sqlite3
-import module_plistlib
-import time
+import ModulePlistlib
 from ClassSqlite import ClassSqlite
 
 DBPath = "/Users/sam/Library/Messages/chat.db"
 SQLite = ClassSqlite(DBPath)
 
 
-def view_data(alldata, field):
-    for item in alldata:
-        # print("📊数据：：：", item[field])
-        res = module_plistlib.parseImessageText(item[field])
-        print("👹结果：", item["ROWID"], res)
+def show_chat(chat_id, msg_id):
+    sql = "SELECT ROWID,properties FROM chat WHERE ROWID={}".format(chat_id)
+    res = SQLite.select(sql)
+    for item in res:
+        res_properties = ModulePlistlib.parseImessageText(item["properties"])
+        print("🎃chat：", chat_id, res_properties)
+
+
+def show_message(chat_id, msg_id):
+    sql = "SELECT ROWID,text,attributedBody,error FROM message WHERE ROWID={}".format(
+        msg_id
+    )
+    res = SQLite.select(sql)
+    for item in res:
+        text = item["text"]
+        attributedBody = ModulePlistlib.parseImessageText(item["attributedBody"])
+        print(
+            "   🎃message：",
+            chat_id,
+            msg_id,
+            "error:",
+            item["error"],
+            text or attributedBody,
+        )
+
+
+def show_message_by_phone(phone):
+    sql = "SELECT * FROM chat WHERE chat_identifier='{}'".format(phone)
+    res = SQLite.select(sql)
+    item = res[0]
+    print(
+        "🥶chat：",
+        item["ROWID"],
+        item["state"],
+        item["chat_identifier"],
+        item["service_name"],
+        item["successful_query"],
+    )
+
+    sql = "SELECT * FROM chat_message_join WHERE chat_id='{}'".format(item["ROWID"])
+    res = SQLite.select(sql)
+    for item in res:
+        chat_id, msg_id = item["chat_id"], item["message_id"]
+        show_message(chat_id, msg_id)
+    print("\n")
 
 
 def init():
-    print("----------------- chat")
-    sql = "SELECT ROWID, properties FROM chat WHERE state=3 LIMIT {};".format(10)
+    sql = "SELECT * FROM chat WHERE state=3 AND service_name='iMessage' AND successful_query=1 ORDER BY ROWID DESC LIMIT 20"
     res = SQLite.select(sql)
-    view_data(res, "properties")
-
-    print("----------------- message")
-    sql = "SELECT ROWID, attributedBody FROM message ORDER BY ROWID DESC LIMIT {};".format(
-        900
-    )
-    res = SQLite.select(sql)
-    view_data(res, "attributedBody")
+    for item in res:
+        show_message_by_phone(item["chat_identifier"])
 
     SQLite.finish()
     print("执行完成")
